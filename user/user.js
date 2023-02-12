ab8527c5221bfb9a255140593359d3bb6b6f34d9
Milind Nair <milindnair261003@gmail.com>
2023-02-13 00:02:09 +0530

    added calendar in user page

---
+++ user/user.js
@@ -0,0 +1,59 @@
+const calendar = document.querySelector('.calendar');
+const monthYear = calendar.querySelector('.month-year');
+const btnPrev = calendar.querySelector('.btn-prev');
+const btnNext = calendar.querySelector('.btn-next');
+const calendarBody = calendar.querySelector('.calendar-body');
+const calendarDates = calendar.querySelector('#calendar-dates');
+
+let date = new Date();
+let currentMonth = date.getMonth();
+let currentYear = date.getFullYear();
+
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
+
+btnPrev.addEventListener('click', () => {
+  currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
+  currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
+  showCalendar(currentMonth, currentYear);
+});
+
+btnNext.addEventListener('click', () => {
+  currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
+  currentMonth = (currentMonth + 1) % 12;
+  showCalendar(currentMonth, currentYear);
+});
+
+function showCalendar(month, year) {
+  let firstDay = new Date(year, month).getDay();
+  let daysInMonth = 32 - new Date(year, month, 32).getDate();
+
+  let tbl = `<tr>`;
+  for (let i = 0; i < firstDay; i++) {
+    tbl += `<td></td>`;
+  }
+  for (let i = 1; i <= daysInMonth; i++) {
+    if ((i + firstDay - 1) % 7 === 0) {
+      tbl += `</tr><tr>`;
+    }
+    tbl += `<td>${i}</td>`;
+  }
+  tbl += `</tr>`;
+  calendarDates.innerHTML = tbl;
+
+  monthYear.innerHTML = months[month] + ' ' + year;
+}
+
+showCalendar(currentMonth, currentYear);
