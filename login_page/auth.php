fdd92b5c698954ca00429df5d37b21be14307c06
Milind Nair <milindnair261003@gmail.com>
2023-03-28 23:58:47 +0530

    added php files for aunthetication

---
+++ login_page/auth.php
@@ -0,0 +1,65 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "eventhub";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+if(isset($_POST['save']))
+{
+  $fullname = $_POST['regname'];
+  $email = $_POST['regemail'];
+  $password = $_POST['regpass'];
+
+  // Prepare statement
+  $stmt = $conn->prepare("INSERT INTO userdata (name, email, password) VALUES (?, ?, ?)");
+  $stmt->bind_param("sss", $fullname, $email, $password);
+
+  if ($stmt->execute()) {
+    echo "New record created successfully";
+    header("Location: ../Home/home.html");
+    exit();
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+
+  // Close statement
+  $stmt->close();
+}
+
+if(isset($_POST['check']))
+{
+  $email = $_POST['regemail'];
+  $password = $_POST['regpass'];
+
+  // Prepare statement
+  $stmt = $conn->prepare("SELECT * FROM userdata WHERE email = ? AND password = ?");
+  $stmt->bind_param("ss", $email, $password);
+
+  if ($stmt->execute()) {
+    $result = $stmt->get_result();
+    if ($result->num_rows > 0) {
+      echo "Login successful";
+      header("Location: ../Home/home.html");
+      exit();
+    } else {
+      echo "Login failed";
+    }
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+
+  // Close statement
+  $stmt->close();
+}
+
+// Close connection
+$conn->close();
+?>
